d1af68be4cd7f271be9d61d90df01f46dcb30dc2
Ismael Lopez <lopez42512@gmail.com>
2021-03-12 15:00:00 -0500

    set up the homepage

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// async function getPokemon() {
+//     try {
+//         let gameResp = await fetch ('https://pokeapi.co/api/v2/pokemon/kanto', {mode: 'cors'});
+//         let games = await gameResp.json();
+//         console.log(games);
+//      } catch (e) {
+//          console.log(e);
+//      }
+// }
+// getPokemon()
+// how to get a poke img
+{/* <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt=""> */}
+
