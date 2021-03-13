ccc108f5ecd84d3e7b517737f81ec91fdc26e993
Ismael Lopez <lopez42512@gmail.com>
2021-03-12 22:07:27 -0500

    finish the pokedex page for gen 1

--- app.js
+++ app.js
@@ -1,43 +1,59 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const app = express();
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', function(req, res) {
-    console.log('I work')
-    // res.sendFile(__dirname + "index.html")
-    io.on('connection', (socket) => {
-        console.log('a user connected');
-        socket.emit('news', { hello: 'world' });
-        console.log('data was recieved');
-      });
-    res.sendFile(__dirname + '/html/index.html')
-})
+app.get("/", function (req, res) {
+  console.log("I work");
+  // res.sendFile(__dirname + "index.html")
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.emit("news", { hello: "world" });
+    console.log("data was recieved");
+  });
+  res.sendFile(__dirname + "/html/index.html");
+});
 
+app.get("/pokedex:region", function (req, res) {
+  console.log("this was meet");
+  const region = req.params.region.slice(1);
+  async function getPokemon() {
+    try {
+      // offset=151&
+      await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+        .then((res) => res.json())
+        .then((data) => {
+            let pokemon = data
+          io.on("connection", (socket) => {
+            console.log(data);
+            socket.emit("pokedex", { region: pokemon });
+            pokemon = []
+            console.log("data was recieved");
+          });
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  getPokemon();
 
-
-app.get('/pokedex:region', function(req,res) {
-    console.log('this was meet');
-    const region = req.params;
-    res.sendFile(__dirname + '/html/pokedex.html')
-})
+  res.sendFile(__dirname + "/html/pokedex.html");
+});
 // app.get('/pokemon')
-app.post('/pokedex', function(req,res) {
-    console.log('post logged');
-    const region = req.body.region
-    console.log(region);
-    res.redirect('/pokedex:' + region)
-})
+app.post("/pokedex", function (req, res) {
+  console.log("post logged");
+  const region = req.body.region;
+  console.log(region);
+  res.redirect("/pokedex:" + region);
+});
 
-http.listen(process.env.PORT || 3000, function (req,res) {
-    console.log("APP listening on PORT 3000");
-})
+http.listen(process.env.PORT || 3000, function (req, res) {
+  console.log("APP listening on PORT 3000");
+});
--- public/js/pokedex.js
+++ public/js/pokedex.js
@@ -1,3 +1,21 @@
-fetch('/pokedex') 
-.then(resp => resp.json())
-.then(data => console.log(data))
+var socket = io();
+socket.on("pokedex", function (data) {
+  // listen to news event raised by the server
+  var pokemon = data.region.results
+  for (var i = 1; i <= pokemon.length; i++) {
+    var div = document.createElement('div');
+    div.setAttribute('class', 'col-lg-2 col-md-3 col-sm-6 cards');
+    var card = document.createElement('div')
+    card.setAttribute('class', 'card');
+    var image = document.createElement('img');
+    image.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`);
+    card.appendChild(image)
+    var pokeName = document.createElement('h3');
+    pokeName.innerHTML = pokemon[i-1].name
+    card.appendChild(pokeName)
+    div.appendChild(card);
+    document.getElementById('pokemonContainer').appendChild(div);
+}
+});
+
+
